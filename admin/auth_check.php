6331d21c4049b859c8f047d23f0905dc8d6e4ba1
Angelo Lunzaga Evangelista III <angelod3rd@gmail.com>
2025-11-29 01:24:59 +0800

    Update auth_check.php

--- admin/auth_check.php
+++ admin/auth_check.php
@@ -1,6 +1,12 @@
 <?php
-if (!isset($_SESSION['admin_id'])) {
-    header("Location: admin_login.php");
-    exit;
+function isAdminLoggedIn() {
+    return isset($_SESSION['admin_id']);
+}
+
+function requireAdminLogin() {
+    if (!isAdminLoggedIn()) {
+        header("Location: admin_login.php");
+        exit;
+    }
 }
 ?>
