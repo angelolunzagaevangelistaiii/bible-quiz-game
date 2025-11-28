809ba22226f66be4abf93a9a3aca954addff2072
Angelo Lunzaga Evangelista III <angelod3rd@gmail.com>
2025-11-29 01:31:32 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -9,16 +9,16 @@ $admin_name = $_SESSION['admin_name'];
 <html>
 <head>
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../public/style.css">
+    <link rel="stylesheet" href="/public/style.css">
 </head>
 <body>
 <div class="container">
     <h2>Welcome, <?= htmlspecialchars($admin_name); ?></h2>
     <ul>
-        <li><a href="add_question.php">Add Question</a></li>
-        <li><a href="manage_questions.php">Manage Questions</a></li>
-        <li><a href="leaderboard_admin.php">View Leaderboard</a></li>
-        <li><a href="admin_logout.php">Logout</a></li>
+        <li><a href="/admin/add_question.php">Add Question</a></li>
+        <li><a href="/admin/manage_questions.php">Manage Questions</a></li>
+        <li><a href="/admin/leaderboard_admin.php">View Leaderboard</a></li>
+        <li><a href="/admin/admin_logout.php">Logout</a></li>
     </ul>
 </div>
 </body>
