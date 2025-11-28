d84f1dfe14dd063555f106ef14bd7a12fecce2bc
Angelo Lunzaga Evangelista III <angelod3rd@gmail.com>
2025-11-29 07:22:52 +0800

    Update delete_question.php

--- admin/delete_question.php
+++ admin/delete_question.php
@@ -1,21 +1,14 @@
 <?php
-session_start();
-require "../config/db.php";
-require "auth_check.php";
+require_once "admin_protect.php";
+require_once "../config/config.php";
 
-if (!isset($_GET['id'])) {
-    header("Location: manage_questions.php");
-    exit;
-}
+if (!isset($_GET['id'])) { header("Location: manage_questions.php"); exit; }
 
 $id = intval($_GET['id']);
-
-// Delete question
-$stmt = $conn->prepare("DELETE FROM questions WHERE id = ?");
+$stmt = $conn->prepare("DELETE FROM questions WHERE id=?");
 $stmt->bind_param("i", $id);
 $stmt->execute();
 $stmt->close();
 
 header("Location: manage_questions.php");
 exit;
-?>
